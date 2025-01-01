7fc6d35c6f5fdc41c53dac8f12a2bb7e1a7db9d1
Danbi <120550679+danbeeS2@users.noreply.github.com>
2025-01-01 22:59:53 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

--- 250101/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
+++ 250101/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -1,36 +1,28 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n').map(line => line.split(' ').map(Number))
-
 const n = input[0][0]
 const arr = input[1]
 
-let maxN = 0
-let idx = 0
+let curEnd = n  // 탐색 범위 끝 (시작은 배열 전체)
 let res = []
 
-for (let i=0; i<n; i++){
-    let num = arr[i]
-    if (maxN < num ) {
-        maxN = num
-        idx = i
+while (curEnd > 0) {
+    let maxN = 0
+    let idx = -1
+
+    // 현재 탐색 범위에서 최댓값 찾기
+    for (let i=0; i<curEnd; i++){
+        if (maxN < arr[i]) {
+            maxN = arr[i]
+            idx = i
+        }
     }
+    // 최댓값의 순서 저장
+    res.push(idx+1)
+
+    // 탐색 범위 왼쪽으로 조정
+    curEnd = idx
+
 }
-
-// 최댓값 초기화
-maxN = 0
-
-// 첫번째 최댓값의 순서 저장
-res.push(idx+1)
-
-for (let i=0; i<res[0]-1; i++){
-    let num = arr[i]
-    if (maxN < num ) {
-        maxN = num
-        idx = i
-    }
-}
-
-// 두번째 최댓값의 순서 저장
-res[0] !== idx+1 ? res.push(idx+1) : ''
 
 console.log(res.join(' '))
