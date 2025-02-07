425660b85c87218f094074c82b74b4d17b90f44f
박단비 <silverwill39@gmail.com>
2025-02-07 16:48:20 +0900

    Lv.1 햄버거 만들기 - 시간 초과

--- Programmers/Lv.1/햄버거_만들기.js
+++ Programmers/Lv.1/햄버거_만들기.js
@@ -1,17 +1,15 @@
 // Lv.1 햄버거 만들기
+// 시간 초과
 function solution(ingredient) {
     let res = 0;
     let totalIngredient = ingredient.join('')
-    let buger = []
     
-    // [반례] 입력: [1,2,3,1,2,3,1,1] 기댓값: 2
+    // [반례] 입력: [1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1, 1],  기댓값: 2
     
-    while(buger.length !== 1){
-        buger = totalIngredient.split('1231')  // 빵-야채-고기-빵로 구분
-        res += buger.length - 1  // 만들어진 버거 개수 세기
-        
-        totalIngredient = buger.join('')  // 재료 정렬
+    while(totalIngredient.includes('1231')){
+        totalIngredient = totalIngredient.replace('1231', '');  // 첫 번째 햄버거 제거
+        res++;  // 햄버거 개수 증가
     }
-       
+    
     return res;
 }
