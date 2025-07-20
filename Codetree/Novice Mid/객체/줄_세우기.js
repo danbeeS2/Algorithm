97984fa05038071f4eef11422b02d275c99eaf14
박단비 <silverwill39@gmail.com>
2025-07-21 00:06:29 +0900

    feat: Solve / 연습 / 객체 정렬 / 줄 세우기

---
+++ Codetree/Novice Mid/객체/줄_세우기.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+// const n = Number(input[0])
+const info = input.slice(1).map((x) => x.split(" ").map(Number));
+
+class Student {
+  constructor(height, weight, number = 0) {
+    this.height = height;
+    this.weight = weight;
+    this.number = number;
+  }
+
+  print() {
+    console.log(this.height, this.weight, this.number);
+  }
+}
+
+const arrayToObject = (arr) => {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    const [h, w] = arr[i];
+    res.push(new Student(h, w, i + 1));
+  }
+  return res;
+};
+
+const sortByTall = (arr) => {
+  return [...arr].sort((a, b) => {
+    if (a.height === b.height) {
+      if (a.weight !== b.weight) return b.weight - a.weight; // 2. 몸무게가 더 큰 학생이 앞에
+      return a.number - b.number; // 3. 번호가 작은 학생이 앞에
+    }
+    return b.height - a.height; // 1. 키가 더 큰 학생이 앞에
+  });
+};
+
+const main = () => {
+  const students = arrayToObject(info);
+  const sorted = sortByTall(students);
+
+  sorted.map((student) => student.print());
+};
+
+main();
