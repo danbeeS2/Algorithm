cc274bb4566819af058f813a45939abba651b7af
박단비 <silverwill39@gmail.com>
2025-07-24 17:38:50 +0900

    refactor: 연습 / 시뮬레이션 1 / 사각형의 총 넓이 2

--- Codetree/Novice Mid/시뮬레이션/사각형의 총 넓이 2.js
+++ Codetree/Novice Mid/시뮬레이션/사각형의 총 넓이 2.js
@@ -7,32 +7,30 @@ const rects = input.slice(1).map(line => line.split(' ').map(Number))
 
 const OFFSET = 100
 
+const creatBoard = (size) => Array.from({length : size}, () => Array(size).fill(0))
+
 const markRectangleArea = (rects) => {
-    const board = new Array(OFFSET * 2).fill().map(() => new Array(OFFSET * 2).fill(0))
-    rects.forEach(([x1, y1, x2, y2]) => {
-        for (let i = x1 + OFFSET; i < x2 + OFFSET; i++){
-            for (let j = y1 + OFFSET; j < y2 + OFFSET; j++){
-                board[i][j] = 1
+    const board = creatBoard(OFFSET * 2)
+
+    for (const [x1, y1, x2, y2] of rects){
+        for (let x = x1 + OFFSET; x < x2 + OFFSET; x++){
+            for (let y = y1 + OFFSET; y < y2 + OFFSET; y++){
+                board[x][y] = 1
             }
         }
-    })
+    }
 
     return board
 }
 
 const countFilledArea = (board) => {
-    let cnt = 0
-    board.forEach((row) => {
-        cnt += row.reduce((acc, cur) => acc + cur, 0)
-    })
-
-    return cnt
+    return board.reduce((totalSum, row) => totalSum + row.reduce((acc, cur) => acc + cur, 0), 0)
 }
 
 const main = () => {
     const board = markRectangleArea(rects)
-    const cnt = countFilledArea(board)
-    console.log(cnt)
+    const totalArea = countFilledArea(board)
+    console.log(totalArea)
 }
 
 main()
