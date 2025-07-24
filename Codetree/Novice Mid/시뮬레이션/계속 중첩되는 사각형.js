7d11ed1090cdd16e5e72944c8185e53c756c12c6
박단비 <silverwill39@gmail.com>
2025-07-24 22:22:36 +0900

    refactor: 연습 / 시뮬레이션 1 / 계속 중첩되는 사각형

--- Codetree/Novice Mid/시뮬레이션/계속 중첩되는 사각형.js
+++ Codetree/Novice Mid/시뮬레이션/계속 중첩되는 사각형.js
@@ -7,6 +7,8 @@ const rects = input.slice(1).map((line) => line.split(" ").map(Number));
 
 const OFFSET = 100;
 const MAX_SIZE = OFFSET * 2;
+const RED = 0;
+const BLUE = 1;
 
 const createBoard = (size) =>
   Array.from({ length: size }, () => Array(size).fill(0));
@@ -14,32 +16,24 @@ const createBoard = (size) =>
 const paintRedAndBlue = (rects) => {
   const board = createBoard(MAX_SIZE);
 
-  const RED = 0;
-  const BLUE = 1;
-
-  for (let i = 0; i < rects.length; i++) {
-    const [x1, y1, x2, y2] = rects[i];
+  rects.forEach(([x1, y1, x2, y2], i) => {
+    const color = i % 2 === 0 ? RED : BLUE;
     for (let y = y1 + OFFSET; y < y2 + OFFSET; y++) {
       for (let x = x1 + OFFSET; x < x2 + OFFSET; x++) {
-        board[x][y] = i % 2 ? BLUE : RED;
+        board[x][y] = color;
       }
     }
-  }
+  });
   return board;
 };
 
-const getBlueRect = (board) => {
-  return board.reduce(
-    (totalSum, row) => totalSum + row.reduce((acc, cur) => acc + cur, 0),
-    0
-  );
+const countBlueArea = (board) => {
+  return board.reduce((sum, row) => sum + row.reduce((a, b) => a + b, 0), 0);
 };
 
 const main = () => {
-  const painted = paintRedAndBlue(rects);
-  const res = getBlueRect(painted);
-
-  console.log(res);
+  const board = paintRedAndBlue(rects);
+  console.log(countBlueArea(board));
 };
 
 main();
