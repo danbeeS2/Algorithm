b54b40522bd20ccab98879e7469f1d88f8f5c366
박단비 <silverwill39@gmail.com>
2025-07-25 13:37:08 +0900

    feat: Solve / 연습 / 시뮬레이션 2 / 만나는 그 순간 (Set 풀이 추가)

--- Codetree/Novice Mid/시뮬레이션/만나는 그 순간.js
+++ Codetree/Novice Mid/시뮬레이션/만나는 그 순간.js
@@ -44,3 +44,65 @@ const main = () => {
 };
 
 main();
+
+
+// 다른 풀이
+// const fs = require("fs")
+// const input = fs.readFileSync(0).toString().trim().split("\n")
+
+// const [n, m] = input[0].split(' ').map(Number)
+// const arr = input.slice(1).map(line => {
+//     const [dir, dist] = line.split(' ')
+//     return [dir, Number(dist)]
+// })
+
+// const arrA = arr.slice(0, n)
+// const arrB = arr.slice(n)
+
+// const OFFSET = 1_000_000
+
+// const checkTime = (arr) => {
+//     const checked = new Map()  // key: 위치, value: 시간
+
+//     let pos = OFFSET
+//     let time = 1
+//     for (const [dir, dist] of arr){
+//         const d = dir === "R" ? 1 : -1
+//         for (let i = 0; i < dist; i++){
+//             pos += d
+//             if(!checked.has(pos)) checked.set(pos, [])
+//             checked.get(pos).push(time++)
+//         }    
+//     }
+
+//     return checked
+// }
+
+
+
+// const getMeetTime = (mapA, mapB) => {
+//     let minTime = Infinity
+
+//     for (const [pos, timesA] of mapA){
+//         if(!mapB.has(pos)) continue
+
+//         const timesB = mapB.get(pos)  // B가 pos에 있었던 시간들
+//         const setA = new Set(timesA)  // A가 pos에 있었던 시간들(Set으로 변환)
+//         for (const time of timesB){
+//             if (time !== 1 && setA.has(time)){
+//                 minTime = Math.min(minTime, time)
+//             }
+//         }
+//     } 
+//     return minTime !== Infinity ? minTime : -1
+// } 
+
+
+// const main = () => {
+//     const mapA = checkTime(arrA)
+//     const mapB = checkTime(arrB)
+
+//     console.log(getMeetTime(mapA, mapB))
+// }
+
+// main()
