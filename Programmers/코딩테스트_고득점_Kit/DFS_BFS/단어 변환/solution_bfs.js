032ef3dafdbd2719d43f000ab658f799b89ddecd
박단비 <silverwill39@gmail.com>
2025-08-16 12:49:00 +0900

    feat: Lv.3 / 단어 변환 / BFS 풀이 추가

---
+++ Programmers/코딩테스트_고득점_Kit/DFS_BFS/단어 변환/solution_bfs.js
@@ -0,0 +1,36 @@
+function solution(begin, target, words) {
+  // 목표 단어가 없으면 단어 변환 불가
+  if (!words.includes(target)) return 0;
+
+  const len = begin.length;
+  const visited = Array(words.length).fill(0);
+
+  const isDiffOne = (a, b) => {
+    let cnt = 0;
+    for (let i = 0; i < len; i++) {
+      if (a[i] !== b[i]) cnt++;
+    }
+
+    return cnt === 1;
+  };
+
+  const bfs = () => {
+    const queue = [[begin, 0]]; // [현재단어, 변환횟수]
+
+    while (queue.length > 0) {
+      const [cur, step] = queue.shift();
+      if (cur === target) return step;
+
+      for (let i = 0; i < words.length; i++) {
+        if (visited[i]) continue;
+        if (!isDiffOne(cur, words[i])) continue;
+
+        visited[i] = true;
+        queue.push([words[i], step + 1]);
+      }
+    }
+    return 0;
+  };
+
+  return bfs();
+}
