d6e8fd0f86d21d2fd78c71bd55640a332c63289d
박단비 <silverwill39@gmail.com>
2025-10-23 12:46:47 +0900

    feat: Lv.1 / 문자열 / 신규 아이디 추천 - 정규표현식 풀이 추가

--- Programmers/Lv.1/신규 아이디 추천.js
+++ Programmers/Lv.1/신규 아이디 추천.js
@@ -1,38 +1,16 @@
 // 2021 KAKAO BLIND RECRUITMENT - Lv.1 신규 아이디 추천
 function solution(new_id) {
-  var answer = "";
-
   let recommend_id = new_id
     .toLowerCase() // 1단계
-    .replace(/[^a-z0-9-_.]/g, ""); // 2단계
-
-  // 3단계 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
-  let tmp = "";
-  for (let i = 0; i < recommend_id.length; i++) {
-    const curr = recommend_id[i];
-    const prev = tmp[tmp.length - 1];
-    if (curr === "." && prev === ".") continue; // 연속된 점이면 건너뜀
-    tmp += curr;
-  }
-  recommend_id = tmp;
-
-  // 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거
-  if (recommend_id[0] === ".") {
-    recommend_id = recommend_id.slice(1);
-  }
-
-  if (recommend_id[recommend_id.length - 1] === ".") {
-    recommend_id = recommend_id.slice(0, -1);
-  }
+    .replace(/[^a-z0-9-_.]/g, "") // 2단계
+    .replace(/\.+/g, ".") // 3단계
+    .replace(/^\.|\.$/g, ""); // 4단계
 
   if (recommend_id === "") recommend_id = "a"; // 5단계
 
   // 6단계 길이가 16자 이상이면, 첫 15개의 문자를 제외 제거
   // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 제거
-  recommend_id = recommend_id.slice(0, 15);
-  if (recommend_id[recommend_id.length - 1] === ".") {
-    recommend_id = recommend_id.slice(0, -1);
-  }
+  recommend_id = recommend_id.slice(0, 15).replace(/\.$/g, "");
 
   // 7단계 길이가 2자 이하라면, new_id의 마지막 문자를 길이가 3이 될 때까지 끝에 추가
   while (recommend_id.length < 3) {
