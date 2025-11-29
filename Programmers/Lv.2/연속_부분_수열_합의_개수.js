8fa2018d5032b51e571e7d958b99a292167cfd14
박단비 <silverwill39@gmail.com>
2025-11-29 20:05:50 +0900

    feat: Lv.2 / 연속 부분 수열 합의 개수 / 슬라이딩 윈도우 (다른 풀이)

--- Programmers/Lv.2/연속_부분_수열_합의_개수.js
+++ Programmers/Lv.2/연속_부분_수열_합의_개수.js
@@ -1,23 +1,24 @@
 // 연속 부분 수열 합의 개수
 function solution(elements) {
-  let answer = 0;
   const n = elements.length;
-
   const set = new Set();
+
+  // 원형 배열을 만들기 위해 두배 확장
+  const arr = [...elements, ...elements];
 
   for (let k = 1; k <= n; k++) {
     let sum = 0;
-    // console.log(set)
-    for (let i = 0; i < n + k; i++) {
-      sum += elements[i % n];
 
-      // k개인 부분 수열을 완성
-      if (i >= k - 1) {
-        set.add(sum);
-        sum -= elements[(i - k + 1 + n) % n]; // 시작값 뺴기 (원형 인덱싱 처리)
-      }
+    // 첫 슬라이딩
+    for (let i = 0; i < k; i++) sum += arr[i];
+    set.add(sum);
+
+    // 슬라이딩
+    for (let i = k; i < n + k; i++) {
+      sum += arr[i]; // 새로운 값 더하고
+      sum -= arr[i - k]; // 이전 값 빼기
+      set.add(sum);
     }
   }
-
   return set.size;
 }
